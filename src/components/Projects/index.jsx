c7e0629a2383b82e19da54c17fcb52b1637f57bf
Heriberto Santana <heriberto.santana@dci-student.org>
2024-08-28 17:18:17 +0200

    no se

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -8,38 +8,49 @@ export function Projects() {
   const { t } = useTranslation();
 
   return (
-    <section className={styles.section}>
-      <h1 className={styles.h1}>{t("nav.projects")}</h1>
-      <aside className={styles.aside}>
-        <img
-          className={styles.img}
-          src={t("projects.project1.image")}
-          alt={t("projects.project1.name")}
-        />
-      </aside>
+    <>
+      <header>
+        <h1 className={styles.h1}>{t("nav.projects")}</h1>
+      </header>
       <main className={styles.main}>
-        <header className={styles.header}>
+        <aside className={styles.aside}>
+          <img
+            className={styles.img}
+            src={t("projects.project1.image")}
+            alt={t("projects.project1.name")}
+          />
+        </aside>
+        <section className={styles.section}>
           <h2 className={styles.h2}>{t("projects.project1.name")}</h2>
+          <ul className={styles.ul}>
+            <li className={styles.li}>
+              <a
+                className={styles.a}
+                href={t("projects.project1.github")}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {t("projects.project1.info")}
+                {t("projects.project1.name")}
+                <FaGithub className={styles.icon} />
+              </a>
+            </li>
+            <li className={styles.li}>
+              <a
+                className={styles.a}
+                href={t("projects.project1.live")}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {t("projects.project1.info")}
+                {t("projects.project1.name")}
+                <FaExternalLinkAlt className={styles.icon} />
+              </a>
+            </li>
+          </ul>
           <p className={styles.p}>{t("projects.project1.description")}</p>
-        </header>
-
-        <ul className={styles.ul}>
-          <li className={styles.li}>
-            <a className={styles.a} href={t("projects.project1.github")} target="_blank" rel="noopener noreferrer">
-              {t("projects.project1.info")}
-              {t("projects.project1.name")}
-              <FaGithub className={styles.icon} />
-            </a>
-          </li>
-          <li className={styles.li}>
-            <a className={styles.a} href={t("projects.project1.live")} target="_blank" rel="noopener noreferrer">
-              {t("projects.project1.info")}
-              {t("projects.project1.name")}
-              <FaExternalLinkAlt className={styles.icon} />
-            </a>
-          </li>
-        </ul>
+        </section>
       </main>
-    </section>
+    </>
   );
 }
