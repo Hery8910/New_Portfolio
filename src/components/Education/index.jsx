ea7485e0853baeb49a5d20b0a46528538189ab7c
Heriberto Santana <heriberto.santana@dci-student.org>
2024-08-30 19:06:36 +0200

    falta cv

--- src/components/Education/index.jsx
+++ src/components/Education/index.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 import styles from "./Education.module.css";
 import { ImHtmlFive2 } from "react-icons/im";
 import { TbBrandCss3 } from "react-icons/tb";
 import { RiJavascriptLine } from "react-icons/ri";
-import { FaReact } from "react-icons/fa";
+import { FaDownload, FaReact } from "react-icons/fa";
 import { FaSass } from "react-icons/fa";
 import { FaLinux } from "react-icons/fa";
 import { FaNodeJs } from "react-icons/fa6";
@@ -22,12 +22,43 @@ import { SiVisualstudio } from "react-icons/si";
 
 export function Education() {
   const { t } = useTranslation();
+  const [isOpen, setIsOpen] = useState(false);
+  function handelOpen() {
+    setIsOpen(true);
+  }
+  function handelClose() {
+    setIsOpen(false);
+  }
 
   return (
     <section className={styles.section}>
       <main className={styles.main}>
         <h2 className={styles.h2}>{t("education.about")}</h2>
         <p className={styles.p}>{t("education.aboutMessage")}</p>
+        <div className={styles.buttons}>
+          <a
+            className={styles.a}
+            href={t("education.cv.link")}
+            download="Heriberto_Santana_CV.pdf"
+          >
+            {t("education.cv.show")} <FaDownload />
+          </a>
+          {isOpen ? (
+            <div className={styles.showPdf}>
+              <button  className={styles.closeButton} onClick={handelClose}>X</button>
+              <iframe
+                className={styles.iframe}
+                src={t("education.cv.link")}
+                width="110%"
+                height="100%"
+              />
+            </div>
+          ) : (
+            <button className={styles.showButton} onClick={handelOpen}>
+              {t("education.cv.download")}
+            </button>
+          )}
+        </div>
       </main>
       <aside>
         <ul className={styles.ul}>
